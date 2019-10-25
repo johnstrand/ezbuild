09b08d9196acdef22808662a30a61a4a3c774fc2
John Strand <john.strand@dekiru.se>
2019-10-25 20:08:52 +0200

    Removed unused helper component

--- src/utils/WithPopover.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ElementType } from "react";
-import { Popover, IPopoverProps } from "@blueprintjs/core";
-
-export const WithPopover = (child: JSX.Element) => {
-    return (popoverProps: IPopoverProps) => (
-        <Popover {...popoverProps}>{child}</Popover>
-    );
-};
