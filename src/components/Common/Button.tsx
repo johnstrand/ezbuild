2e4271e9bce6b0abb93622eca85473935538c69d
John Strand <john.strand@dekiru.se>
2019-10-26 20:59:40 +0200

    Made tooltip optional

--- src/components/Common/Button.tsx
+++ src/components/Common/Button.tsx
@@ -3,8 +3,11 @@ import { Button as BPButton, IButtonProps, Tooltip } from "@blueprintjs/core";
 
 export const Button = (
     props: IButtonProps &
-        React.HTMLAttributes<HTMLButtonElement> & { tooltip: string }
+        React.HTMLAttributes<HTMLButtonElement> & { tooltip?: string }
 ) => {
+    if (!props.tooltip) {
+        return <BPButton {...props}>{props.children}</BPButton>;
+    }
     return (
         <Tooltip inheritDarkTheme content={props.tooltip}>
             <BPButton {...props}>{props.children}</BPButton>
