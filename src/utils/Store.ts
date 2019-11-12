f33cfffce6f3bf656f56e6bc0ec28e6b33f7fc59
John Strand <john.strand@dekiru.se>
2019-11-12 21:36:42 +0100

    Removed unused export

--- src/utils/Store.ts
+++ src/utils/Store.ts
@@ -1,63 +1,58 @@
 import createStore from "utils/Squawk"; //"squawk-react";
 import {
-    Project,
-    BuildDefinition,
-    AzureTenant,
-    Organization
+  Project,
+  BuildDefinition,
+  AzureTenant,
+  Organization
 } from "utils/ApiTypes";
 import {
-    ProjectService,
-    BuildService,
-    RepositoryService,
-    ReleaseService,
-    ProfileService,
-    AccountService
+  ProjectService,
+  BuildService,
+  RepositoryService,
+  ReleaseService,
+  ProfileService,
+  AccountService
 } from "utils/Api/Interfaces";
 import { Account } from "msal";
 import { getAccount } from "utils/Auth";
 import Api from "./Api/Api";
 
 interface AppState {
-    authority: string;
-    tenantFilter: AzureTenant[];
-    tenants: AzureTenant[];
-    tenantId: string | null;
-    account: Account;
-    organizations: Organization[];
-    organizationId: string | null;
-    projects: Project[];
-    projectId: string | null;
-    buildDefinitions: BuildDefinition[];
-    projectService: ProjectService;
-    buildService: BuildService;
-    repositoryService: RepositoryService;
-    releaseService: ReleaseService;
-    profileService: ProfileService;
-    accountService: AccountService;
+  authority: string;
+  tenantFilter: AzureTenant[];
+  tenants: AzureTenant[];
+  tenantId: string | null;
+  account: Account;
+  organizations: Organization[];
+  organizationId: string | null;
+  projects: Project[];
+  projectId: string | null;
+  buildDefinitions: BuildDefinition[];
+  projectService: ProjectService;
+  buildService: BuildService;
+  repositoryService: RepositoryService;
+  releaseService: ReleaseService;
+  profileService: ProfileService;
+  accountService: AccountService;
 }
 
-export const {
-    action,
-    useSquawk,
-    pending,
-    usePending,
-    update,
-    get: getStoreValue
-} = createStore<AppState>({
-    authority: "common",
-    tenantFilter: JSON.parse(localStorage.getItem("tenantFilter")!) || [],
-    tenants: [],
-    tenantId: null,
-    account: getAccount(),
-    organizations: [],
-    organizationId: null,
-    projects: [],
-    projectId: null,
-    buildDefinitions: [],
-    projectService: Api.projectService,
-    buildService: Api.buildService,
-    repositoryService: Api.repositoryService,
-    releaseService: Api.releaseService,
-    profileService: Api.profileService,
-    accountService: Api.accountService
+export const { action, useSquawk, pending, usePending, update } = createStore<
+  AppState
+>({
+  authority: "common",
+  tenantFilter: JSON.parse(localStorage.getItem("tenantFilter")!) || [],
+  tenants: [],
+  tenantId: null,
+  account: getAccount(),
+  organizations: [],
+  organizationId: null,
+  projects: [],
+  projectId: null,
+  buildDefinitions: [],
+  projectService: Api.projectService,
+  buildService: Api.buildService,
+  repositoryService: Api.repositoryService,
+  releaseService: Api.releaseService,
+  profileService: Api.profileService,
+  accountService: Api.accountService
 });
