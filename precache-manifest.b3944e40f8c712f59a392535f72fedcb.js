5179c9f12c61e756ab66cf96be58f26872e241e2
John Strand <john.strand@dekiru.se>
2020-01-12 18:49:29 +0100

    Updates

---
+++ precache-manifest.b3944e40f8c712f59a392535f72fedcb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf538f689638c2dd6109033b53777c1",
+    "url": "/ezbuild/index.html"
+  },
+  {
+    "revision": "86d9d9a2ada85bb837ac",
+    "url": "/ezbuild/static/css/2.cf0bcc05.chunk.css"
+  },
+  {
+    "revision": "1593fa744e30a56684cb",
+    "url": "/ezbuild/static/css/main.634ef570.chunk.css"
+  },
+  {
+    "revision": "86d9d9a2ada85bb837ac",
+    "url": "/ezbuild/static/js/2.f8a2d411.chunk.js"
+  },
+  {
+    "revision": "1593fa744e30a56684cb",
+    "url": "/ezbuild/static/js/main.397b3ee5.chunk.js"
+  },
+  {
+    "revision": "a2bb9af5d33d30c064c9",
+    "url": "/ezbuild/static/js/runtime-main.8aba6bc0.js"
+  },
+  {
+    "revision": "b62ce8316edcf6cd91b261e1b6db07ea",
+    "url": "/ezbuild/static/media/icons-16.b62ce831.eot"
+  },
+  {
+    "revision": "e0aef47b375af9a5e8a601ea48e74a45",
+    "url": "/ezbuild/static/media/icons-16.e0aef47b.ttf"
+  },
+  {
+    "revision": "e2458cd59cf9786664a5fb610d6f19f1",
+    "url": "/ezbuild/static/media/icons-16.e2458cd5.woff"
+  },
+  {
+    "revision": "748778e0d622390feff65d4086a7cf13",
+    "url": "/ezbuild/static/media/icons-20.748778e0.ttf"
+  },
+  {
+    "revision": "95f6660eb5ef987149ab2e79dfe47046",
+    "url": "/ezbuild/static/media/icons-20.95f6660e.eot"
+  },
+  {
+    "revision": "9f1e61641e0a1d3f70d4bbaa66f5122c",
+    "url": "/ezbuild/static/media/icons-20.9f1e6164.woff"
+  }
+]);
